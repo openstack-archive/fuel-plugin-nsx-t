993b31d17d21add165498732e6df9778e2be43dd
Artem Savinov <asavinov@mirantis.com>
2016-08-16 09:20:42 +0300

    Added puppet providers for add nodes to NSX-T
    * Added puppet provider to register node in NSX-T fabric
    * Added puppet provider to add node as transport node in NSX-T
    
    Example:
    
    nsxt_add_to_fabric { 'Register node on MP':
      managers => 'https://172.16.0.245, https://172.16.0.249:443',
      username => 'admin',
      password => 'Qwer!1234',
      ensure => present,
    }
    
    nsxt_create_transport_node { 'Add transport node':
      managers => 'https://172.16.0.245, https://172.16.0.249:443',
      username => 'admin',
      password => 'Qwer!1234',
      ensure => present,
      host_switch_name => 'overlay-switch',
      host_switch_profile_ids => [{'key' => 'UplinkHostSwitchProfile',
                                 'value' => '37e67225-d5fa-49d6-bcb7-0637b1064e4b'}],
      pnics => [{'device_name' => 'enp0s6', 'uplink_name' => 'uplink'}],
      static_ip_pool_id => '6861b43b-d9bf-43af-b379-47ba768c2920',
      transport_zone_id => 'd45a6956-e8cb-4f35-ae85-9a40a7b9bd87',
    }
    
    Change-Id: I017ec6c120effb10bce2c8f2382d4a85c60f5feb

---
+++ deployment_scripts/puppet/modules/nsxt/lib/puppet/provider/nsxt_add_to_fabric/nsxt_add_to_fabric.rb
@@ -0,0 +1,89 @@
+require File.join(File.dirname(__FILE__),'..', 'nsxtutils')
+
+Puppet::Type.type(:nsxt_add_to_fabric).provide(:nsxt_add_to_fabric, :parent => Puppet::Provider::Nsxtutils) do
+ 
+  # need this for work nsxtcli method, otherwise not work
+  commands :nsxcli => 'nsxcli'
+
+  def create
+    debug("Attempting to register a node")
+    # need define for return error from cycle
+    out_reg = ''
+    @resource[:managers].each do |manager|
+      thumbprint = get_manager_thumbprint(manager, ca_file = @resource[:ca_file])
+      if not thumbprint.empty?
+        # 12 retry x 15 sleep time = 3 minutes timeout
+        retry_count = 12
+        while retry_count > 0
+          out_reg = nsxtcli("join management-plane #{manager} username #{@resource[:username]} thumbprint #{thumbprint} password #{@resource[:password]}")
+          if exists?
+            notice("Node added to NSX-T fabric")
+            return true
+          else
+            retry_count -= 1
+            sleep 15
+          end
+        end
+      end
+    end
+    raise Puppet::Error,("\nNode not add to NSX-t fabric:\n #{out_reg}\n")
+  end
+
+  def exists?
+    connected_managers = nsxtcli("get managers")
+    if connected_managers.include? "Connected"
+      node_id = get_node_id
+      if not node_id.empty?
+        @resource[:managers].each do |manager|
+          if check_node_registered(manager, node_id)
+            debug("Node '#{node_id}' connected and registered on '#{manager}'")
+            return true
+          end
+        end
+      end
+    end
+    debug("Node NOT registered on NSX-T manager")
+    return false
+  end
+
+  def destroy
+    debug("Attempting to unregister a node")
+    # need define for return error from cycle
+    out_unreg = ''
+    @resource[:managers].each do |manager|
+      thumbprint = get_manager_thumbprint(manager, ca_file = @resource[:ca_file])
+      if not thumbprint.empty?
+        # 12 retry x 15 sleep time = 3 minutes timeout
+        retry_count = 12
+        while retry_count > 0
+          out_unreg = nsxtcli("detach management-plane #{manager} username #{@resource[:username]} thumbprint #{thumbprint} password #{@resource[:password]}")
+          if not exists?
+            notice("Node deleted from NSX-T fabric")
+            return true
+          else
+            retry_count -= 1
+            sleep 15
+          end
+        end
+      end
+    end
+    raise Puppet::Error,("\nNode not deleted from NSX-t fabric: \n #{out_unreg}\n")
+  end
+
+  def check_node_registered(manager, node_id)
+    api_url = "https://#{manager}/api/v1/fabric/nodes/#{node_id}/state"
+    response = get_nsxt_api(api_url, @resource[:username], @resource[:password], @resource[:ca_file])
+    if not response.to_s.empty? 
+      if response['state'] == 'success'
+        debug("Node '#{node_id}' registered on '#{manager}'")
+        return true
+      else
+        debug("Node NOT registered on '#{manager}', details:\n#{response['details']}")
+      end
+    else
+      debug("Node NOT registered on '#{manager}'")
+    end
+    return false
+  end
+
+end
---
+++ deployment_scripts/puppet/modules/nsxt/lib/puppet/provider/nsxtutils.rb
@@ -0,0 +1,169 @@
+# require ruby-rest-client, ruby-json
+
+require 'rest-client'
+require 'json'
+require 'socket'
+require 'openssl'
+
+class Puppet::Provider::Nsxtutils < Puppet::Provider
+
+  def get_nsxt_api(api_url, username, password, ca_file, timeout=5)
+    retry_count = 3
+    begin
+      if ca_file.to_s.empty?
+        response = RestClient::Request.execute(method: :get, url: api_url, timeout: timeout, user: username, password: password, verify_ssl: OpenSSL::SSL::VERIFY_NONE) 
+      else
+        response = RestClient::Request.execute(method: :get, url: api_url, timeout: timeout, user: username, password: password, verify_ssl: OpenSSL::SSL::VERIFY_PEER, ssl_ca_file: ca_file)
+      end
+      response_hash = JSON.parse(response.body)
+      return response_hash
+    rescue Errno::ECONNREFUSED
+      notice("\nCan not get response from #{api_url} - 'Connection refused', try next if exist\n")
+      return ""
+    rescue Errno::EHOSTUNREACH
+      notice("\nCan not get response from #{api_url} - 'No route to host', try next if exist\n")
+      return ""
+    rescue => error
+      retry_count -= 1
+      if retry_count > 0
+        sleep 10
+        retry
+      else
+        raise Puppet::Error,("\nCan not get response from #{api_url} :\n#{error.message}\n#{JSON.parse(error.response)['error_message']}\n")
+      end
+    end
+  end
+
+  def post_nsxt_api(api_url, username, password, request, ca_file, timeout=5)
+    retry_count = 3
+    begin
+      if ca_file.to_s.empty?
+        response = RestClient::Request.execute(method: :post, url: api_url, payload: request, timeout: timeout, user: username, password: password, verify_ssl: OpenSSL::SSL::VERIFY_NONE,headers: {'Content-Type' => 'application/json'})
+      else
+        response = RestClient::Request.execute(method: :post, url: api_url, payload: request, timeout: timeout, user: username, password: password, verify_ssl: OpenSSL::SSL::VERIFY_PEER, ssl_ca_file: ca_file, headers: {'Content-Type' => 'application/json'})
+      end
+      response_hash = JSON.parse(response.body)
+      return response_hash
+    rescue Errno::ECONNREFUSED
+      notice("\nCan not get response from #{api_url} - 'Connection refused', try next if exist\n")
+      return ""
+    rescue Errno::EHOSTUNREACH
+      notice("\nCan not get response from #{api_url} - 'No route to host', try next if exist\n")
+      return ""
+    rescue => error
+      retry_count -= 1
+      if retry_count > 0
+        sleep 10
+        retry
+      else
+        raise Puppet::Error,("\nCan not get response from #{api_url} :\n#{error.message}\n#{JSON.parse(error.response)['error_message']}\n")
+      end
+    end
+  end
+
+  def delete_nsxt_api(api_url, username, password, ca_file, timeout=5)
+    retry_count = 3
+    begin
+      if ca_file.to_s.empty?
+        response = RestClient::Request.execute(method: :delete, url: api_url, timeout: timeout, user: username, password: password, verify_ssl: OpenSSL::SSL::VERIFY_NONE)
+      else
+        response = RestClient::Request.execute(method: :delete, url: api_url, timeout: timeout, user: username, password: password, verify_ssl: OpenSSL::SSL::VERIFY_PEER, ssl_ca_file: ca_file)
+      end
+      # if http code not 20x - rest client raise exception
+      return true
+    rescue Errno::ECONNREFUSED
+      notice("\nCan not get response from #{api_url} - 'Connection refused', try next if exist\n")
+      return false
+    rescue Errno::EHOSTUNREACH
+      notice("\nCan not get response from #{api_url} - 'No route to host', try next if exist\n")
+      return false
+    rescue => error
+      retry_count -= 1
+      if retry_count > 0
+        sleep 10
+        retry
+      else
+        raise Puppet::Error,("\nCan not get response from #{api_url} :\n#{error.message}\n")
+      end
+    end
+  end
+
+  def get_node_id
+    uuid = nsxtcli("get node-uuid")
+    if uuid =~ /\A[\da-f]{32}\z/i or uuid =~ /\A(urn:uuid:)?[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}\z/i
+      return uuid
+    end
+    notice("Cannot get node uuid")
+    return ""
+  end
+
+  def get_transport_node_id(manager, node_id, ca_file)
+    api_url = "https://#{manager}/api/v1/transport-nodes"
+    response = get_nsxt_api(api_url, @resource[:username], @resource[:password], @resource[:ca_file])
+    if not response.to_s.empty?
+      response['results'].each do |node|
+        if node['node_id'] == node_id
+          return node['id']
+        end
+      end
+    end
+    notice("Cannot get transport node id")
+    return ""
+  end
+
+  def nsxtcli(cmd)
+    out_cli = nsxcli(['-c', cmd]).to_s.strip
+    debug("cmd out:\n #{out_cli}")
+    return out_cli
+  end
+
+  def get_manager_host_port(manager)
+    # scheme does not take into account, NSX-T 1.0 supports only https
+    manager =~ /(https:\/\/)?(?<host>[^:]+):?(?<port>\d+)?/
+    manager_host_port = {}
+    manager_host_port['host']= Regexp.last_match[:host]
+    port = Regexp.last_match[:port]
+    port = 443 if port.to_s.empty?
+    manager_host_port['port'] = port
+    return manager_host_port
+  end
+
+  def get_manager_thumbprint(manager, timeout=5, ca_file)
+    manager_host_port = get_manager_host_port(manager)
+    host = manager_host_port['host']
+    port = manager_host_port['port']
+    retry_count = 3
+    begin
+      tcp_client = TCPSocket.new(host, port, timeout)
+      ssl_context = OpenSSL::SSL::SSLContext.new()
+      if ca_file.to_s.empty?
+        ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      else
+        ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        ssl_context.ca_file = ca_file
+      end
+      ssl_client = OpenSSL::SSL::SSLSocket.new(tcp_client, ssl_context)
+      ssl_client.connect
+      cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
+      ssl_client.sysclose
+      tcp_client.close
+      tp = OpenSSL::Digest::SHA256.new(cert.to_der)
+      return OpenSSL::Digest::SHA256.new(cert.to_der).to_s
+    rescue Errno::ECONNREFUSED
+      notice("\nCan not get 'thumbprint' from #{host}:#{port} - 'Connection refused', try next if exist\n")
+      return ""
+    rescue Errno::EHOSTUNREACH
+      notice("\nCan not get 'thumbprint' from #{host}:#{port} - 'No route to host', try next if exist\n")
+      return ""
+    rescue => error
+      retry_count -= 1
+      if retry_count > 0
+        sleep 5
+        retry
+      else
+        raise Puppet::Error,("\nCan not get thumbprint from #{host}:#{port} :\n#{error.message}\n")
+      end
+    end
+  end
+
+end
