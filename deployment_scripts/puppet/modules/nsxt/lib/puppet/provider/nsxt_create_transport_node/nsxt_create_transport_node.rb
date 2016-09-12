18b92360c568ddadb5a4df5099d7edd1c85a7dc5
Igor Zinovik <izinovik@mirantis.com>
2016-09-12 11:13:11 +0300

    Move away from 'text_list' to 'textarea'
    text_list type is not supported by stable version of fuel plugin builder
    (plugin build process fails due to validation check).
    
    Example value produces multiple text fields for each character written
    to 'value' which cannot be removed or edited.
    
    Change-Id: Ief60cef42d10d48bc61d77cd9ca21424679002fe

--- deployment_scripts/puppet/modules/nsxt/lib/puppet/provider/nsxt_create_transport_node/nsxt_create_transport_node.rb
+++ deployment_scripts/puppet/modules/nsxt/lib/puppet/provider/nsxt_create_transport_node/nsxt_create_transport_node.rb
@@ -143,7 +143,7 @@ Puppet::Type.type(:nsxt_create_transport_node).provide(:nsxt_create_transport_no
     if not pnics.instance_of? Array
       pnics = [pnics]
     end
-    pnics.each do |pnic_pair|
+    pnics.each_line do |pnic_pair|
       device,uplink = pnic_pair.split(':')
       result_pnic_pairs.push({'device_name' => device.strip, 'uplink_name' => uplink.strip})
     end
